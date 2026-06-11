a53abefb3eca6526b82c479a0d8f3715cd410e51
KozyrevDA <34125850+KozyrevDA@users.noreply.github.com>
2026-06-11 16:22:20 +0400

    Fix: remove duplicate ImagePicker import

--- composeApp/src/androidMain/kotlin/ImagePicker.android.kt
+++ composeApp/src/androidMain/kotlin/ImagePicker.android.kt
@@ -1,7 +1,6 @@
 package utils
 
 import utils.ImagePicker
-import ImagePicker
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
